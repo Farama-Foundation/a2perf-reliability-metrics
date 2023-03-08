24d0e01bd7ca6a05d6e362bea531b88e0b4ad518
Ikechukwu Uchendu <iuchendu@g.harvard.edu>
2023-03-07 20:56:34 -0500

    making reliability into a module for rl perf

--- rl_reliability_metrics/metrics/__init__.py
+++ rl_reliability_metrics/metrics/__init__.py
@@ -13,3 +13,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from rl_perf.metrics.reliability.rl_reliability_metrics.metrics.metrics_offline import *
+from rl_perf.metrics.reliability.rl_reliability_metrics.metrics.metrics_online import *
